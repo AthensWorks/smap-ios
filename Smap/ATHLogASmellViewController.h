78cf097acf7c0f63947cd66255a9a3e039612477
Ben Lachman <ben@nicemohawk.com>
2013-02-21 23:18:32 -0500

    Adding Log a Smell View Controller

---
+++ Smap/ATHLogASmellViewController.h
@@ -0,0 +1,16 @@
+//
+//  ATHLogASmellViewController.h
+//  Smap
+//
+//  Created by Ben Lachman on 2/21/13.
+//  Copyright (c) 2013 Athensworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ATHSmell.h"
+
+@interface ATHLogASmellViewController : UITableViewController <UITextFieldDelegate>
+
+@property ATHSmell *smell;
+
+@end
