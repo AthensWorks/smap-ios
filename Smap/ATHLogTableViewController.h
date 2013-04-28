fe4adcfa61cf67487382d03c240e3ecd9fc5d9fc
Ben Lachman <ben@acaciatreesoftware.com>
2013-04-28 00:54:50 -0400

    Adding a tableview for reactions to be selected.

---
+++ Smap/ATHLogTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ATHLogTableViewController.h
+//  Smap
+//
+//  Created by Ben Lachman on 4/28/13.
+//  Copyright (c) 2013 Athensworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATHLogTableViewController : UITableViewController
+
+- (IBAction)unwindFromReactions:(UIStoryboardSegue *)segue;
+
+@end
